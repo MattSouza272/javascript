37d1735e43329c920fd6dedee5ecd7d15ac446ef
MattSouza272 <97816266+MattSouza272@users.noreply.github.com>
2022-02-03 13:58:30 -0300

    aula 11 JS

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'BR'
+console.log(`Sendo nativo de ${pais} `)
+if(pais == 'BR'){
+    console.log('você é Brasileiro!')
+} else{
+    console.log('você é Estrangeiro!')
+}
+console.log('Bem vindo ao Brasil!')
