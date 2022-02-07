6f8ed90c48c189c58e4c223cb51349d12e720b68
MattSouza272 <97816266+MattSouza272@users.noreply.github.com>
2022-02-07 09:22:00 -0300

    ex 14 js att

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,18 +3,25 @@ function contar(){
     var fn = Number(document.getElementById('fn').value)
     var passo = Number(document.getElementById('passo').value)
     var msg = document.getElementById('msg')
-    if(st < 0){
-        msg.innerHTML = `Início inválido!<br> Início deve ser maior do que ${fn}`
-    } else if(fn <= st){
-        msg.innerHTML = `Final inválido!<br> Final deve ser maior do que ${st}`
-    } else if(passo <= 0){
-        msg.innerHTML = `Passo inválido!<br> Passo deve ser maior do que ${passo}`
+    if(st == 0 || fn == 0){
+        msg.innerHTML = 'Dados inválidos'
     } else{
         msg. innerHTML = '<p>Contando...</p>'
-        msg.innerHTML += ` ${st} `
-        while(st < fn && (st+passo) < fn){
-            st += passo
-            msg.innerHTML += ` ${st} `
+        if(passo <= 0){
+            window.alert('Passo Inválido! Considerando passo = 1')
+            passo = 1
         }
+        //contagem crescente
+        if(st < fn){
+            for(var c = st; c <= fn; c += passo){
+                msg.innerHTML += `${c} \u{1F449}`
+            }
+        //contagem decrescente
+        } else{
+            for(var c = st; c >= fn; c -= passo){
+                msg.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        msg.innerHTML += `\u{1f3c1}`
     }
 }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,25 +1,17 @@
 function tab(){
     var num = Number(document.getElementById('num').value)
     var msg = document.getElementById('msg')
-    var t = num*1
-    msg.innerHTML += `${num} x 1 = ${t} <br>`
-    var t = num*2
-    msg.innerHTML += `${num} x 2 = ${t} <br>`
-    var t = num*3
-    msg.innerHTML += `${num} x 3 = ${t} <br>`
-    var t = num*4
-    msg.innerHTML += `${num} x 4 = ${t} <br>`
-    var t = num*5
-    msg.innerHTML += `${num} x 5 = ${t} <br>`
-    var t = num*6
-    msg.innerHTML += `${num} x 6 = ${t} <br>`
-    var t = num*7
-    msg.innerHTML += `${num} x 7 = ${t} <br>`
-    var t = num*8
-    msg.innerHTML += `${num} x 8 = ${t} <br>`
-    var t = num*9
-    msg.innerHTML += `${num} x 9 = ${t} <br>`
-    var t = num*10
-    msg.innerHTML += `${num} x 10 = ${t} <br>`
-    
+    var tab = document.getElementById('tab')
+    if(num == 0){
+        msg.innerHTML = 'Favor inserir um número válido'
+    } else{
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
